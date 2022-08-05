980bb00afd93fbb7a5dc8e82b277ec23d5ca9564
Juri Donvito <juri.donvito@gmail.com>
2022-08-05 10:06:57 +0200

    feat: Add some test

--- __tests__/First.test.ts
+++ __tests__/First.test.ts
@@ -1,6 +1,18 @@
 import JsOperation from '../index';
 const operation = new JsOperation();
 
-test('First test', () => {
+test('Addition', () => {
     expect(operation.addition(0.3, 0.1)).toBe(0.4);
+});
+
+test('Subtraction', () => {
+    expect(operation.subtraction(0.3, 0.1)).toBe(0.2);
+});
+
+test('Multiplication', () => {
+    expect(operation.multiplication(0.07, 100)).toBe(7);
+});
+
+test('Division', () => {
+    expect(operation.division(5.3, 0.1)).toBe(53);
 });
